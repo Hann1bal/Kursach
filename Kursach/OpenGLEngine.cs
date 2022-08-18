6370362cacacdb41939f86772e349447d4a54eda
milenium856 <milenium856@gmail.com>
2022-08-18 20:47:19 +0300

    For Adeptus Akimus

---
+++ Kursach/OpenGLEngine.cs
@@ -0,0 +1,344 @@
+﻿using System.Drawing;
+using GameTry.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Kursach;
+
+public sealed class OpenGLEngine : GameWindow
+{
+    private Matrix4 _view;
+    private Matrix4 _projection;
+    private double _time;
+
+    private readonly float[] _vertices =
+    {
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+    };
+
+    float[] vertices =
+    {
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+    };
+
+    private readonly uint[] _indices =
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    private int _elementBufferObject;
+
+    private int _vertexBufferObject;
+
+    private int _vertexArrayObject;
+
+    private int _elementBufferObject2;
+
+    private int _vertexBufferObject2;
+
+    private int _vertexArrayObject2;
+
+    private Shader _shader;
+
+    private Texture _texture;
+
+    private Texture _texture2;
+
+    private Camera _camera;
+    private bool _firstMove = true;
+    private Vector2 _lastPos;
+
+    public OpenGLEngine(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(
+        gameWindowSettings, nativeWindowSettings)
+    {
+    }
+
+    protected override void OnLoad() // Вызывается при первоначальной загрузке
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // We enable depth testing here. If you try to draw something more complex than one plane without this,
+        // you'll notice that polygons further in the background will occasionally be drawn over the top of the ones in the foreground.
+        // Obviously, we don't want this, so we enable depth testing. We also clear the depth buffer in GL.Clear over in OnRenderFrame.
+        GL.Enable(EnableCap.DepthTest);
+
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices,
+            BufferUsageHint.StaticDraw);
+
+
+        _elementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices,
+            BufferUsageHint.StaticDraw);
+
+
+        _vertexArrayObject2 = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject2);
+
+        _vertexBufferObject2 = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject2);
+        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+
+        _elementBufferObject2 = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject2);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices,
+            BufferUsageHint.StaticDraw);
+
+
+        // shader.vert has been modified. Take a look at it after the explanation in OnRenderFrame.
+        _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+        _shader.Use();
+
+        var vertexLocation = _shader.GetAttribLocation("aPosition");
+        GL.EnableVertexAttribArray(vertexLocation);
+        GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+        var texCoordLocation = _shader.GetAttribLocation("aTexCoord");
+        GL.EnableVertexAttribArray(texCoordLocation);
+        GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float),
+            3 * sizeof(float));
+
+        _texture = Texture.TextureLoader(@"Resources/container.png");
+        _texture.Use(TextureUnit.Texture0);
+
+        _texture2 = Texture.TextureLoader("Resources/back.png");
+        _texture2.Use(TextureUnit.Texture1);
+
+        _shader.SetInt("texture0", 0);
+        _shader.SetInt("texture1", 1);
+
+        // For the view, we don't do too much here. Next tutorial will be all about a Camera class that will make it much easier to manipulate the view.
+        // For now, we move it backwards three units on the Z axis.
+        _view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
+
+        // For the matrix, we use a few parameters.
+        //   Field of view. This determines how much the viewport can see at once. 45 is considered the most "realistic" setting, but most video games nowadays use 90
+        //   Aspect ratio. This should be set to Width / Height.
+        //   Near-clipping. Any vertices closer to the camera than this value will be clipped.
+        //   Far-clipping. Any vertices farther away from the camera than this value will be clipped.
+        _projection =
+            Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), Size.X / (float) Size.Y, 0.1f,
+                100.0f);
+
+        // Now, head over to OnRenderFrame to see how we setup the model matrix.
+        _camera = new Camera(Vector3.UnitZ * 3, Size.X / (float) Size.Y);
+
+        // We make the mouse cursor invisible and captured so we can have proper FPS-camera movement.
+        CursorState = CursorState.Grabbed;
+    }
+
+    // Вызывается при изменение размеров окна
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        if (!IsFocused) // Check to see if the window is focused
+        {
+            return;
+        }
+
+        var input = KeyboardState;
+
+        if (input.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+
+        const float cameraSpeed = 11.5f;
+        const float sensitivity = 0.2f;
+
+        if (input.IsKeyDown(Keys.W))
+        {
+            _camera.Position += _camera.Front * cameraSpeed * (float) e.Time; // Forward
+        }
+
+        if (input.IsKeyDown(Keys.S))
+        {
+            _camera.Position -= _camera.Front * cameraSpeed * (float) e.Time; // Backwards
+        }
+
+        if (input.IsKeyDown(Keys.A))
+        {
+            _camera.Position -= _camera.Right * cameraSpeed * (float) e.Time; // Left
+        }
+
+        if (input.IsKeyDown(Keys.D))
+        {
+            _camera.Position += _camera.Right * cameraSpeed * (float) e.Time; // Right
+        }
+        // if (input.IsKeyDown(Keys.Space))
+        // {
+        //     _camera.Position += _camera.Up * cameraSpeed * (float)e.Time; // Up
+        // }
+        // if (input.IsKeyDown(Keys.LeftShift))
+        // {
+        //     _camera.Position -= _camera.Up * cameraSpeed * (float)e.Time; // Down
+        // }
+
+        // Get the mouse state
+        var mouse = MouseState;
+
+        if (_firstMove) // This bool variable is initially set to true.
+        {
+            _lastPos = new Vector2(mouse.X, mouse.Y);
+            _firstMove = false;
+        }
+        else
+        {
+            // Calculate the offset of the mouse position
+            var deltaX = mouse.X - _lastPos.X;
+            var deltaY = mouse.Y - _lastPos.Y;
+            _lastPos = new Vector2(mouse.X, mouse.Y);
+
+            // Apply the camera pitch and yaw (we clamp the pitch in the camera class)
+            _camera.Yaw += deltaX * sensitivity;
+            _camera.Pitch -= deltaY * sensitivity; // Reversed since y-coordinates range from bottom to top
+        }
+    }
+
+    // In the mouse wheel function, we manage all the zooming of the camera.
+    // This is simply done by changing the FOV of the camera.
+    protected override void OnMouseWheel(MouseWheelEventArgs e)
+    {
+        base.OnMouseWheel(e);
+
+        _camera.Fov -= e.OffsetY;
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+
+        GL.Viewport(0, 0, Size.X, Size.Y);
+        _camera.AspectRatio = Size.X / (float) Size.Y;
+    }
+
+    // Вызывается при отрисовке очередного кадра
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        Title = $"Retry again ass flame: (Vsync: {VSync}) FPS: {1f / e.Time:0}, pos:{_camera.Position}";
+        base.OnRenderFrame(e);
+
+        _time += 4.0 * e.Time;
+
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        GL.BindVertexArray(_vertexArrayObject);
+
+        _texture.Use(TextureUnit.Texture0);
+        _texture2.Use(TextureUnit.Texture1);
+        _shader.Use();
+
+        var model = Matrix4.Identity * Matrix4.CreateRotationX((float) MathHelper.DegreesToRadians(_time));
+        _shader.SetMatrix4("model", model);
+        _shader.SetMatrix4("view", _camera.GetViewMatrix());
+        _shader.SetMatrix4("projection", _camera.GetProjectionMatrix());
+
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+
+        GL.BindVertexArray(_vertexArrayObject2);
+        _texture.Use(TextureUnit.Texture0);
+        _texture2.Use(TextureUnit.Texture1);
+        _shader.Use();
+
+        var model2 = Matrix4.Identity * Matrix4.CreateRotationX((float) MathHelper.DegreesToRadians(_time));
+        _shader.SetMatrix4("model", model2);
+        _shader.SetMatrix4("view", _camera.GetViewMatrix());
+        _shader.SetMatrix4("projection", _camera.GetProjectionMatrix());
+
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+        SwapBuffers();
+    }
+}
---
+++ Kursach/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Kursach;
+
+internal class Programm
+{
+    private static void Main(string[] args)
+    {
+        var nativeWindowSettings = new NativeWindowSettings
+        {
+            Size = new Vector2i(1000, 1000),
+            Title = "FirstGameEngineTry",
+            Flags = ContextFlags.Offscreen
+        };
+        using (var game = new OpenGLEngine(GameWindowSettings.Default, nativeWindowSettings))
+        {
+            game.VSync = VSyncMode.Adaptive;
+            game.Run();
+        }
+    }
+}
